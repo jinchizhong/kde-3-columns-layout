d8990a37b04b0825590c52c06b5700541a2eae2f
Chizhong Jin <pjincz@gmail.com>
2020-08-10 17:56:15 +0800

    add two new shortcuts

--- contents/main.js
+++ contents/main.js
@@ -1,9 +1,13 @@
-function layout_to(horz, vert) {
+function layout_to(horz, vert, wid) {
+  if (!wid) {
+    wid = 1;
+  }
+
   var area = workspace.clientArea(KWin.MaximizeArea, workspace.activeClient);
   var rc = workspace.activeClient.geometry;
 
   rc.x = area.x + area.width / 3 * horz;
-  rc.width = area.width / 3;
+  rc.width = area.width / 3 * wid;
 
   if (vert === undefined || vert === null) {
     rc.y = 0;
@@ -27,3 +31,12 @@ registerShortcut("Layout Top Right", "Move current window to top of right column
 registerShortcut("Layout Bottom Left", "Move current window to bottom of left column", "Meta+Num+1", function() { layout_to(0, 1); });
 registerShortcut("Layout Bottom Center", "Move current window to bottom of center column", "Meta+Num+2", function() { layout_to(1, 1); });
 registerShortcut("Layout Bottom Right", "Move current window to bottom of right column", "Meta+Num+3", function() { layout_to(2, 1); });
+
+registerShortcut("Layout Left 2", "Move current window to left 2 columns", "Meta+Shift+Num+Left", function() { layout_to(0, null, 2); });
+registerShortcut("Layout Right 2", "Move current window to right 2 columns", "Meta+Shift+Num+Right", function() { layout_to(1, null, 2); });
+
+//registerShortcut("Layout Top Left", "Move current window to top of left 2 columns", "Meta+Shift+Num+7", function() { layout_to(0, 0, 2); });
+//registerShortcut("Layout Top Right", "Move current window to top of right 2 columns", "Meta+Shift+Num+9", function() { layout_to(1, 0, 2); });
+
+//registerShortcut("Layout Bottom Left", "Move current window to bottom of left 2 columns", "Meta+Shift+Num+1", function() { layout_to(0, 1, 2); });
+//registerShortcut("Layout Bottom Right", "Move current window to bottom of right 2 columns", "Meta+Shift+Num+3", function() { layout_to(1, 1, 2); });
