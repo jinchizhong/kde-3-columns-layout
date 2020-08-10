a5d25c00bd6d447d1284d279b9a74f5ae41314b5
Chizhong Jin <pjincz@gmail.com>
2020-08-10 13:30:04 +0800

    add readme.md and change description

---
+++ README.md
@@ -0,0 +1,29 @@
+# Install
+
+```bash
+$ git clone https://github.com/jinchizhong/kde-3-columns-layout
+$ cd kde-3-columns-layout
+$ make install
+```
+
+Open `KDE System Settings` => `Workspace` => `Window Management` => Enable `Three Columns Layout`
+
+# Usage
+
+## Default shortcuts
+
+`Meta+Num4`: Move window to left column
+`Meta+Num5`: Move window to center column
+`Meta+Num6`: Move window to right column
+
+`Meta+Num7`: Move window to top of left column
+`Meta+Num8`: Move window to top of center column
+`Meta+Num9`: Move window to top of right column
+
+`Meta+Num1`: Move window to bottom of left column
+`Meta+Num2`: Move window to bottom of center column
+`Meta+Num3`: Move window to bottom of right column
+
+## Change shortcuts
+
+Open `KDE System Settings` => `Workspace` => `Shortcuts` => `Global Shortcuts` => `Other Shortcuts` => `KWin`
--- contents/main.js
+++ contents/main.js
@@ -16,14 +16,14 @@ function layout_to(horz, vert) {
   workspace.activeClient.geometry = rc;
 }
 
-registerShortcut("Layout Left", "Layout current window to left", "Meta+Num+4", function() { layout_to(0); });
-registerShortcut("Layout Center", "Layout current window to center", "Meta+Num+5", function() { layout_to(1); });
-registerShortcut("Layout Right", "Layout current window to right", "Meta+Num+6", function() { layout_to(2); });
+registerShortcut("Layout Left", "Move current window to left column", "Meta+Num+4", function() { layout_to(0); });
+registerShortcut("Layout Center", "Move current window to center column", "Meta+Num+5", function() { layout_to(1); });
+registerShortcut("Layout Right", "Move current window to right column", "Meta+Num+6", function() { layout_to(2); });
 
-registerShortcut("Layout Top Left", "Layout current window to top left", "Meta+Num+7", function() { layout_to(0, 0); });
-registerShortcut("Layout Top Center", "Layout current window to top center", "Meta+Num+8", function() { layout_to(1, 0); });
-registerShortcut("Layout Top Right", "Layout current window to top right", "Meta+Num+9", function() { layout_to(2, 0); });
+registerShortcut("Layout Top Left", "Move current window to top of left column", "Meta+Num+7", function() { layout_to(0, 0); });
+registerShortcut("Layout Top Center", "Move current window to top of center column", "Meta+Num+8", function() { layout_to(1, 0); });
+registerShortcut("Layout Top Right", "Move current window to top of right column", "Meta+Num+9", function() { layout_to(2, 0); });
 
-registerShortcut("Layout Bottom Left", "Layout current window to bottom left", "Meta+Num+1", function() { layout_to(0, 1); });
-registerShortcut("Layout Bottom Center", "Layout current window to bottom center", "Meta+Num+2", function() { layout_to(1, 1); });
-registerShortcut("Layout Bottom Right", "Layout current window to bottom right", "Meta+Num+3", function() { layout_to(2, 1); });
+registerShortcut("Layout Bottom Left", "Move current window to bottom of left column", "Meta+Num+1", function() { layout_to(0, 1); });
+registerShortcut("Layout Bottom Center", "Move current window to bottom of center column", "Meta+Num+2", function() { layout_to(1, 1); });
+registerShortcut("Layout Bottom Right", "Move current window to bottom of right column", "Meta+Num+3", function() { layout_to(2, 1); });
